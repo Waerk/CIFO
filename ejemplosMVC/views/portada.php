c720ec2fd7e6853752da6f107d608f022ca287e8
Marc Montferrer <marcmontferrer@gmail.com>
2025-02-19 13:51:28 +0100

    MVC

--- ejemplosMVC/views/portada.php
+++ ejemplosMVC/views/portada.php
@@ -14,10 +14,7 @@
 			<li><a href="index.php?controlador=libro/create">Nuevo libro</a></li>
 		</menu>
 		<h2>Bienvenido...</h2>
-			<p>Esta es la portada del primer ejemplo de gestion de libros de la biblioteca.</p>+
-			<p>Este ejemplo es un <b>MVC sencillo</b>, todas las peticiones pasarn por el <b>index.php</b>, que actuara de dispatcher y gestionara todos los errores.</p>
-		<div class="centrado">
-			<a class="button" href="index.php?controlador=libro/list">Lista de Libros</a>
-		</div>
+			<p>Esta es la portada del primer ejemplo de gestion de libros de la biblioteca.</p>
+			<p>Este ejemplo es un <b>MVC sencillo</b>, todas las peticiones pasarn por el <b>index.php</b>, que actuara de dispatcher y gestionara todos los errores.</p>			
 	</body>
 </html>
